35c8fe3293f69481643ffedb93c2723ad1f11fef
HectorRosas76 <hecrosas@uniboyaca.edu.co>
2026-06-12 17:04:28 -0500

    Hector [Arreglos]

--- Microservicios/ms-reservas/app/presentation/Routers/endpointRes.php
+++ Microservicios/ms-reservas/app/presentation/Routers/endpointRes.php
@@ -1,15 +1,15 @@
-//<?php
-//
-//use App\Presentation\Repositories\TestRepository;
-//use Psr\Http\Message\ResponseInterface as Response;
-//use Psr\Http\Message\ServerRequestInterface as Request;
-//use Slim\App;
-//
-//
-//return function (App $app) {
-//    $app->get('/4', function (Request $request, Response $response, $args) {
-//        $controller = new TestRepository();
-//        return $controller->hola($request, $response);
-//    });
-//   //$app->get('/4', [TestRepository::class, 'hola']);
-//};
+<?php
+
+use App\Presentation\Repositories\TestRepository;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\App;
+
+
+return function (App $app) {
+    $app->get('/4', function (Request $request, Response $response, $args) {
+        $controller = new TestRepository();
+        return $controller->hola($request, $response);
+    });
+   //$app->get('/4', [TestRepository::class, 'hola']);
+};
--- Microservicios/ms-reservas/public/index.php
+++ Microservicios/ms-reservas/public/index.php
@@ -1,29 +1,16 @@
 <?php
 
-//use Slim\Factory\AppFactory;
-//
-//require __DIR__ . '/../../vendor/autoload.php';
-//
-//$endpointRes = require __DIR__ . '/../app/Presentation/Routers/endpointRes.php';
-//
-//$app = AppFactory::create();
-//
-//$endpointRes($app);
-//
-//$app->run();
-
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+$endpointRes = require __DIR__ . '/../app/Presentation/Routers/endpointRes.php';
+
 $app = AppFactory::create();
 
-$app->get('/4', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
-    return $response;
-});
+$endpointRes($app);
 
 $app->run();
+
+
 
