2aa6254cf82f171d900e09226a02d893c572a92c
HectorRosas76 <hecrosas@uniboyaca.edu.co>
2026-06-12 16:40:01 -0500

    Hector [Reintegracion de servicios]

--- Microservicios/ms-reservas/app/presentation/Routers/endpointRes.php
+++ Microservicios/ms-reservas/app/presentation/Routers/endpointRes.php
@@ -7,13 +7,12 @@ use Slim\App;
 
 
 return function (App $app) {
-//$app->get('/4', function (Request $request, Response $response, $args) {
-//    $controller = new TestRepository();
-//    return $controller->default($request, $response);
-//});
-$app->get('/4', [TestRepository::class, 'hola']);
+       var_dump(class_exists(\App\Presentation\Repositories\TestRepository::class));
+    die();
 
-
+    $app->get('/4', function (Request $request, Response $response, $args) {
+        $controller = new TestRepository();
+        return $controller->default($request, $response);
+    });
+   //$app->get('/4', [TestRepository::class, 'default']);
 };
-
-
