d3cdae3e289caca1c3b5166d00d6dead360801de
HectorRosas76 <hecrosas@uniboyaca.edu.co>
2026-06-11 23:28:37 -0500

    Hector[Comprobasion backend]

--- Microservicios/ms-reservas/public/index.php
+++ Microservicios/ms-reservas/public/index.php
@@ -1,11 +1,15 @@
 <?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
 require __DIR__ . '/../../vendor/autoload.php';
-require __DIR__ . '/../app/config/database4.php';
 
+$app = AppFactory::create();
 
-
-$app->get('/', function (request $request, response $response, $args) {
-    $response->getBody()->write("Welcome to the Reservas API");
+$app->get('/4', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Hello world!");
     return $response;
 });
+
 $app->run();
